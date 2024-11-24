eceac67acb935ec5bf31a62ac3221dfb3ea97900
Djave17 <dsancheza@uamv.edu.ni>
2024-11-24 16:47:46 -0600

    Adicion de metodo para registrar asistencia en clase Estudiante

--- MyHoursUAM/Estructuras/Estudiante.cs
+++ MyHoursUAM/Estructuras/Estudiante.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MyHours_UAMApp.Estructuras
 {
@@ -16,8 +17,41 @@ namespace MyHours_UAMApp.Estructuras
         public string cifEstudiante { get; set; }
         public string contraseñaEstudiante { get; set; }
         
+       
+        public List<string> eventosAsistidos { get; private set; } // IDs de eventos asistidos
         public Asistencia asistencia { get; set; }
+
+        // Constructor
+        public Estudiante()
+        {
+            eventosAsistidos = new List<string>();
+            asistencia = new Asistencia();
+        }
+
+        // Método para registrar asistencia y contar las horas y partidos realizados
+        public bool RegistrarAsistencia(Evento evento,bool asistio)
+        {
+            if (evento == null)
+                throw new ArgumentNullException("El evento no puede ser nulo."); //Validar que el evento no sea nulo.
+
+            if (eventosAsistidos.Contains(evento.idEvento)) //Validar que el evento no haya sido registrado previamente
+            {
+                Console.WriteLine($"El estudiante ya asistió al evento {evento.nombreEvento}.");
+                MessageBox.Show("El estudiante ya asistió a este evento.","Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false; // Ya asistió al evento
+            }
+
+            // Registrar el evento y actualizar datos
+            eventosAsistidos.Add(evento.idEvento);
+            asistencia.horasCompletadas += evento.cantidadConvalidar;
+            asistencia.cantidadDePartidosAsistidos++;
+            Console.WriteLine($"Asistencia registrada para el evento {evento.nombreEvento}.");
+            MessageBox.Show("Asistencia registrada correctamente.", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return true; // Asistencia registrada correctamente
+        }
     }
 
-
 }
+
+
+
